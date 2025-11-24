f6c9ee3f6ce1fe3dd6784d840bd61883fea25106
Kenny Wan <kennethzwan@gmail.com>
2025-11-24 13:06:01 -0600

    Added header to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { Header } from './components/header'
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-24">
-      <h1 className="text-4xl font-bold">kennywan.me</h1>
-      <p className="mt-4">Coming soon...</p>
-    </main>
+    <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+        <Header />
+        <main className="">
+          <h1 className="text-4xl font-bold">kennywan.me</h1>
+          <p className="mt-4">Coming soon...</p>
+        </main>
+    </div>
   );
 }
