0e59c2ac162ec07f1457d666c799332f62eb87cc
Kenny Wan <kennethzwan@gmail.com>
2025-11-24 18:00:47 -0600

    Added Vercal analytics

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,5 +1,6 @@
 import { headerNavLinks } from 'configs/header-nav-links';
-
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function BlogLayout({
   children,
@@ -7,13 +8,17 @@ export default function BlogLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
-      <div className="flex h-screen flex-col justify-between">
-        <main className="mb-auto">{children}</main>
-        <footer className="py-10 text-center text-sm text-gray-500">
-          © 2026 Kenny Wan
-        </footer>
+    <>
+      <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+        <div className="flex h-screen flex-col justify-between">
+          <main className="mb-auto">{children}</main>
+          <footer className="py-10 text-center text-sm text-gray-500">
+            © 2026 Kenny Wan
+          </footer>
+        </div>
       </div>
-    </div>
+      <Analytics />
+      <SpeedInsights />
+    </>
   )
 }
